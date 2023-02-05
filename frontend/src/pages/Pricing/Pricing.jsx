55338e6e22157df3e36d77d88ed60c60587be762
timadigwe <timothyadigwe2019@gmail.com>
2023-02-05 22:22:45 +0100

    Pricing page

--- frontend/src/pages/Pricing/Pricing.jsx
+++ frontend/src/pages/Pricing/Pricing.jsx
@@ -1,6 +1,20 @@
 import React from "react";
-import { pricingImage1, pricingImage2 } from "../../assets";
+import {
+  pricingImage1,
+  pricingImage2,
+  bankPayouts,
+  buyActive,
+  gipperpaylogo,
+  gipperfiActive,
+} from "../../assets";
 import PricingCard from "./components/PricingCard";
+import {
+  BANKPAYOUTS,
+  CRYPTOPAYOUTS,
+  NATIVETOKENPAYOUT,
+  GIPPERFIPAYOUT,
+} from "./PricingData";
+import SignUp from "../Home/homeComponents/SignUp";
 
 function Pricing() {
   return (
@@ -18,7 +32,61 @@ function Pricing() {
         </div>
       </div>
       <div className="w-full h-full ">
-        <PricingCard />
+        <PricingCard
+          title="Bank payouts"
+          data={BANKPAYOUTS}
+          header="Region"
+          img={bankPayouts}
+          color="#D3F1F8"
+        />
+        <PricingCard
+          title="Crypto payouts"
+          data={CRYPTOPAYOUTS}
+          header="Network"
+          img={buyActive}
+          color="#FCD19C"
+        />
+        <PricingCard
+          title="Native token payouts"
+          data={NATIVETOKENPAYOUT}
+          header="Type"
+          img={gipperpaylogo}
+          color="#CECCD4"
+        />
+        <PricingCard
+          title="GipperFi payouts"
+          data={GIPPERFIPAYOUT}
+          header="Type"
+          img={gipperfiActive}
+          color="#FED8D0"
+        />
+      </div>
+      <div className=" lg:flex lg:flex-row lg:w-[38.25rem] lg:m-auto lg:gap-[5rem]  ">
+        <div className="flex-col justify-center p-[2.5rem] bg-secondary-50 rounded-[3.125rem] text-center ">
+          <h1 className="font-Jost not-italic font-big text-m leading-[2.0625rem] items-center text-whiteText  ">
+            Buy crypto
+          </h1>
+          <h1 className="font-Jost not-italic font-regular text-s leading-[1.4375rem] items-center text-whiteText mt-[2.5rem] ">
+            Transaction Fee
+          </h1>
+          <h1 className="font-Jost not-italic font-medium text-2xl leading-[3.625rem] items-center text-whiteText mt-[1.25rem] ">
+            $2
+          </h1>
+        </div>
+        <div className="flex-col justify-center p-[2.5rem] bg-primary-light rounded-[3.125rem] text-center ">
+          <h1 className="font-Jost not-italic font-big text-m leading-[2.0625rem] items-center text-primary-main  ">
+            Sell crypto
+          </h1>
+          <h1 className="font-Jost not-italic font-regular text-s leading-[1.4375rem] items-center text-primary-main mt-[2.5rem] ">
+            Transaction Fee
+          </h1>
+          <h1 className="font-Jost not-italic font-medium text-2xl leading-[3.625rem] items-center text-primary-main mt-[1.25rem] ">
+            Dynamic
+          </h1>
+        </div>
+      </div>
+      <div>
+        <SignUp />
       </div>
     </div>
   );
--- frontend/src/pages/Pricing/components/PricingCard.jsx
+++ frontend/src/pages/Pricing/components/PricingCard.jsx
@@ -1,35 +1,44 @@
 import { Divider } from "antd";
-import { bankPayouts } from "../../../assets";
 import React from "react";
 
-function PricingCard() {
+function PricingCard({ data, title, header, img, color }) {
+  console.log(data);
   return (
-    <div className="lg:w-[59.375rem] py-[2.5rem] px-[5rem] items-center ">
-      <div className="lg:w-[18.75rem] lg:h-[5rem] flex flex-grid py-[1.46875rem] pl-[1.90625rem]  rounded-[1.25rem] items-center bg-[#D3F1F8] ">
-        <div className="lg:w-[2.93rem] lg:h-[2.34375rem] mr-[1.875rem] ">
-          <img src={bankPayouts} alt="" className="" />
+    <div className="lg:w-[59.375rem] py-[2.5rem] px-[5rem] lg:mt-[5rem]  lg:m-auto items-center ">
+      <div
+        className="lg:w-[21.375rem] lg:h-[5rem] flex flex-grid py-[1.46875rem] pl-[1.90625rem]  rounded-[1.25rem] items-center "
+        style={{ backgroundColor: color }}
+      >
+        <div className="lg:w-[2.93rem] lg:h-[2.34375rem] mr-[0.625rem] ">
+          <img src={img} alt="" className="w-full h-full object-contain" />
         </div>
-        <h1 className="font-Jost not-italic font-big text-m leading-[2.0625rem] text-secondary-main items-center ">
-          BankPayouts
+        <h1 className="lg:w-[14.5rem] lg:h-[2.0625rem] font-Jost not-italic font-big text-m leading-[2.0625rem] text-secondary-main items-center ">
+          {title}
         </h1>
       </div>
-      <div className=" flex flex-row justify-between items-center">
+      <div className=" flex flex-row justify-between p-[0] lg:mt-[1.875rem] ">
         <h1 className="font-Jost not-italic font-big text-sm leading-[1.6875rem] text-secondary-main items-center ">
-          Region
+          {header}
         </h1>
-        <h1>Fee</h1>
+        <h1 className="font-Jost not-italic font-big text-sm leading-[1.6875rem] text-secondary-main items-center ">
+          Fee
+        </h1>
       </div>
       <Divider />
-      <div className=" flex flex-row justify-between items-center">
-        <h1>Region</h1>
-        <h1>Fee</h1>
-      </div>
-      <Divider />
-      <div className=" flex flex-row justify-between items-center">
-        <h1>Region</h1>
-        <h1>Fee</h1>
-      </div>
-      <Divider />
+      {data.map((item, index) => (
+        <>
+          {" "}
+          <div className=" flex flex-row justify-between items-center">
+            <h1 className="font-Jost not-italic font-regular text-s leading-[1.4375rem] text-secondary-main items-center ">
+              {item.name}
+            </h1>
+            <h1 className="font-Jost not-italic font-regular text-s leading-[1.4375rem] text-secondary-main items-center ">
+              {item.fee}
+            </h1>
+          </div>
+          <Divider />
+        </>
+      ))}
     </div>
   );
 }
