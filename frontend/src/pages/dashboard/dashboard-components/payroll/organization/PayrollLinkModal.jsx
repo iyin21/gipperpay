db52a22fd285cbf01a58fa901c14464c680850ed
Mary <marieshix98@gmail.com>
2023-05-24 16:07:53 +0300

    show employee modal

--- frontend/src/pages/dashboard/dashboard-components/payroll/organization/PayrollLinkModal.jsx
+++ frontend/src/pages/dashboard/dashboard-components/payroll/organization/PayrollLinkModal.jsx
@@ -5,10 +5,11 @@ import { useDispatch } from "react-redux";
 import { MdOutlineAttachment } from "react-icons/md";
 import Close from "../../../../../assets/closeIcon.png";
 import { RxCopy } from "react-icons/rx";
+import Invoice from "../invoice/Invoice";
 import stakeBackdrop from "../../../../../widgets/Modal/stake/stakeBackdrop";
-import EmployeeModal from "./EmployeeModal";
-import { setActivePayrollPage } from "../../../../../redux/payrollSlice";
-import OrganizationProfile from "./OrganizationProfile";
+import EmployeeModal from "./EmployeeModal"
+import { setActivePayrollPage, setFreelancePage } from "../../../../../redux/payrollSlice";
+
 
 export const dropIn = {
   hidden: {
@@ -18,27 +19,44 @@ export const dropIn = {
   visible: {
     y: "0",
     opacity: 1,
-    transition: {
-      duration: 0.1,
-      type: "spring",
-      damping: 25,
-      stiffness: 500,
-    },
+    
   },
   exit: {
     y: "100vh",
     opacity: 0,
   },
 };
-const PayrollLinkModal = ({ openModal, handleModal }) => {
-  const [showEmployeeModal, setShowEmployeeModal] = useState(false);
+const PayrollLinkModal = ({ openModal, setOpenModal, setShowOpenModal, showLinkModal, setShowLinkModal, checker }) => {
+  const [showEmployee, setShowEmployee] = useState(false);
+  const [showInvoice, setShowInvoice] = useState(false)
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const handleNavigate = () => {
-    setShowEmployeeModal(true);
-    handleModal();
-    //dispatch(setActivePayrollPage(false))
+  
+    if(checker){
+     setTimeout(()=>{
+      setShowLinkModal(false)
+     },2000000)
+    setShowInvoice(true)
+     
+     
+    }else {
+      console.log('Hello world')
+     
+      setTimeout(()=>{
+        setOpenModal(false)
+        console.log('Heyy')
+       },2000000)
+       setShowEmployee(true)
+      
+     
+    }
+   
   };
+  if(!openModal) return
+  console.log(showEmployee)
+  console.log(openModal)
+ 
   return (
     <>
       <div className="fixed mt-[100px] flex justify-center w-ful h-full backdrop-blur z-50 top-0 left-0 right-0   md:inset-0 h-modal md:max-h-full">
@@ -47,10 +65,10 @@ const PayrollLinkModal = ({ openModal, handleModal }) => {
           initial="hidden"
           animate="visible"
           exit="exit"
-          className="p-6 w-[50%] h-[10rem] mx-auto lg:w-[30rem] lg:mx-0 bg-[#ffffff] border-2  shadow-sm  pb-40 lg:pb-28 rounded-[20px]"
+          className="p-6 w-[100%] h-full lg:h-[10rem] mx-auto lg:w-[30rem] lg:mx-0 bg-[#ffffff] border-2  shadow-sm  pb-40 lg:pb-28 rounded-[20px]"
         >
           <div className=" flex justify-between h-auto items-center ">
-            <p className="flex gap-4 w-[60%]h-auto items-center cursor-pointer text-secondary-50">
+            <p className="flex gap-4 w-full lg:w-[60%] h-auto items-center cursor-pointer text-secondary-50">
               <MdOutlineAttachment size="25" color="#090023" />
               <span className="font-medium text-l font-Jost leading-10">
                 Link generated
@@ -59,16 +77,16 @@ const PayrollLinkModal = ({ openModal, handleModal }) => {
             <img
               src={Close}
               alt="close"
-              onClick={handleModal}
+              onClick={() =>setOpenModal(false)}
               className="w-[20px] h-[20px]"
             />
           </div>
-          <div className="flex justify-between mt-3 gap-1">
-            <p className="font-regular text-s leading-7 w-[60%]">
+          <div className="flex-row lg:flex justify-between mt-3 gap-1">
+            <p className="font-regular text-s leading-7 w-full lg:w-[60%]">
               Share this link with your staff to add them to your payroll
             </p>
             <div
-              onClick={() =>navigate('/employeeForm')}
+              onClick={handleNavigate}
               className=" cursor-pointer text-primary-main bg-primaryLight flex px-1 w-[8rem] h-[3rem] items-center m-auto justify-center"
             >
               <span>Copy link</span>
@@ -77,12 +95,13 @@ const PayrollLinkModal = ({ openModal, handleModal }) => {
           </div>
         </motion.div>
       </div>
-      {showEmployeeModal && (
-        <OrganizationProfile
-          showOrgModal={showEmployeeModal}
-          setShowOrgModal={setShowEmployeeModal}
+      {showEmployee && (
+        <EmployeeModal
+          showEmployeeModal={showEmployee}
+          setShowEmployeeModal={setShowEmployee}
         />
       )}
+      {showInvoice && <Invoice showInvoice={showInvoice} setShowInvoice={setShowInvoice}/>}
     </>
   );
 };
