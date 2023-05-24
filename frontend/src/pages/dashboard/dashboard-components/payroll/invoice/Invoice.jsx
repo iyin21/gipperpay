6200c7f16a2c2b0cc3dd31d9da5d53b972de31de
Mary <marieshix98@gmail.com>
2023-05-24 16:06:20 +0300

    setup invoice pages

---
+++ frontend/src/pages/dashboard/dashboard-components/payroll/invoice/Invoice.jsx
@@ -0,0 +1,21 @@
+import React,{useState} from 'react'
+import ModalBackDrop from '../../../../../widgets/Modal/ModalBackDrop'
+import Payment from './Payment';
+import ConnectWallet from './ConnectWallet';
+const Invoice = ({showInvoice, setShowInvoice}) => {
+    const [stepInvoice, setStepInvoice] = useState(0);
+    const handleNext = () => {
+        setStepInvoice(prev => prev + 1);
+      };
+    
+      const handlePrevious = () => {
+        setStepInvoice(prev => prev - 1);
+      };
+  return (
+    <ModalBackDrop stepInvoice={stepInvoice} showInvoice={showInvoice} handlePrevious={handlePrevious} setShowInvoice={setShowInvoice} type="invoice" stepper={true}>
+       <div className='py-5'> { <ConnectWallet  handleNext={handleNext} stepInvoice={stepInvoice}/>}</div>
+    </ModalBackDrop>
+  )
+}
+
+export default Invoice
