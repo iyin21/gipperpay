aaa4d4d869e4ac0de146f5de8a8528bbea2e622a
iyn21 <iyinoluwa.oluleke@gmail.com>
2023-06-17 00:28:44 +0100

    chore: navigate link icon to the payment link page

--- frontend/src/pages/dashboard/dashboard-components/products/components/productCard.jsx
+++ frontend/src/pages/dashboard/dashboard-components/products/components/productCard.jsx
@@ -1,7 +1,9 @@
 import { MdDeleteForever, MdEdit, MdOutlineLink } from "react-icons/md";
 import Progress from "../../../../../assets/progress.svg";
+import { useNavigate } from 'react-router-dom'
 
 const ProductCard=({image, name, price, itemsLeft, description, handleClick})=>{
+    const navigate=useNavigate();
     return(
         <div className="w-full">
             <img src={image} alt="" className="w-full rounded-t-[20px] object-cover" onClick={handleClick} />
@@ -9,9 +11,9 @@ const ProductCard=({image, name, price, itemsLeft, description, handleClick})=>{
             <div className="flex mt-2 justify-between ">
                 <p className="text-secondary-main font-medium text-s">{name}</p>
                 <div className="grid grid-cols-3 gap-2 text-s">
-                    <MdDeleteForever color="#A4A4A4" size="20px"/>
-                    <MdEdit color="#A4A4A4" size="20px"/>
-                    <MdOutlineLink color="#FA3C12" size="20px"/>
+                    <MdDeleteForever color="#A4A4A4" size="20px" className="cursor-pointer"/>
+                    <MdEdit color="#A4A4A4" size="20px" className="cursor-pointer"/>
+                    <MdOutlineLink color="#FA3C12" size="20px" className="cursor-pointer" onClick={()=>navigate("/payment-link")}/>
                 </div>
             </div>
             <p className="text-secondary-main font-medium text-m mt-1">{price}</p>
